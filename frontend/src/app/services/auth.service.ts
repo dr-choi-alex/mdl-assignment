0478cc65ef4b4eff46af38e2b2b9b91beb778afc
COGjunseoksong <junseok.song@cognex.com>
2022-01-24 16:43:12 +0900

    [UPDATE] Frontend 업데이트

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,12 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from 'express';
+import { UserInfo } from '../interface/ec-template.interface';
+
+const USER_INFO_KEY = 'user-info'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { 
+  isloggedin = false;
+
+  private setLocalStorage(key: string, value: any) {
+    localStorage.setItem(key, JSON.stringify(value));
   }
+
+  private getLocalStorage(key: string) {
+    return JSON.parse(localStorage.getItem(key));
+  }
+
+  private removeLocalStorage(key: string) {
+    localStorage.removeItem(key);
+  }
+  
+  constructor(
+  ) {}
+  
   getUserDetails() {
       return localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')||'{}') : null;
   }
@@ -15,12 +35,30 @@ export class AuthService {
       localStorage.setItem(variableName, data);
   }
 
-  getToken() {
-      return localStorage.getItem('token');
+
+  getUser(): any {
+    const userID = window.localStorage.getItem(USER_INFO_KEY);
+    if (userID) {
+      console.log(userID)
+      this.isloggedin = true;
+      return JSON.parse(userID);
+    }
+
+    return {};
   }
 
-  clearStorage() {
-      localStorage.clear();
-  
+  getUserInfo(){
+    return this.getLocalStorage(USER_INFO_KEY);
   }
+
+  saveUserInfo(data: UserInfo){
+    this.removeLocalStorage(USER_INFO_KEY);
+    this.setLocalStorage(USER_INFO_KEY, data);
+  }
+
+  logout() :void{
+    this.removeLocalStorage(USER_INFO_KEY);
+    this.isloggedin = false;
+  }
+
 }
